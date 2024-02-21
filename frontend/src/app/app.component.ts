9a07b2b9f53cf2cf8c88482523c36c5eda900834
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-02-21 16:52:11 +0000

    feat: added a button for the add to do

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HlmInputDirective } from '../components/ui-input-helm/src/lib/hlm-input.directive';
+import { InputButtonComponent } from '../components/button/addToDoInput.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HlmInputDirective,
+    InputButtonComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'frontend';
